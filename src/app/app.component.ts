9114e93590b0fba81c42985624eb1bf1723f5aae
MihailoBojic <mihailo.bojic@digitalcontrol.me>
2023-06-05 15:28:29 +0200

    fatch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor (private http: HttpClient) {}
   title = 'memoires-project';
+
+
+  fetchData() {
+    this.http.get('https://digitalcontrol.slack.com/files/USP96HCLV/F05AW10MZ45/activities-modified.json').subscribe((data) => {
+      console.log(data);
+});
+}
 }
--- src/app/single-activities/single-activities.component.ts
+++ src/app/single-activities/single-activities.component.ts
@@ -6,25 +6,30 @@ import { DataService } from '../data.service';
 @Component({
   selector: 'app-single-activities',
   templateUrl: './single-activities.component.html',
-  styleUrls: ['./single-activities.component.css']
+  styleUrls: ['./single-activities.component.css'],
 })
-
-
-export class SingleActivitiesComponent  implements OnInit {
-constructor(private http: HttpClient,
-  private route: ActivatedRoute,
-  private router: Router,
-  private service: DataService){
-  }
-
+export class SingleActivitiesComponent implements OnInit {
   activities: any[] | any;
+  activity: any;
+
+  constructor(
+    private http: HttpClient,
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: DataService
+  ) {}
 
   ngOnInit(): void {
-     this.route.queryParams
-      .subscribe(params => {
-        console.log(params);
+    this.route.queryParams.subscribe((params) => {
+
+      this.activities = this.service.getData();
+      this.activity = this.activities.filter((obj: { id: any }) => {
+        return obj.id == params['id'];
+      });
+
+      if (this.activity.length > 0) {
+        this.activity = this.activity[0];
       }
-    );
-    this.activities = this.service.getData()
+    });
   }
 }
